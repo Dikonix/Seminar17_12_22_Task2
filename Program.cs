24dd49f678e04b1b071ada39ae15309feb104313
Diana Brik <di.brik@yandex.kz>
2022-12-20 20:11:15 +0600

    Seminar 17.12.22 Task 2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int firstNumber;
+int secondNumber;
+
+try
+{
+    Console.Write("Введите первое число: ");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Введено не целое число");
+    return;
+}
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"max = {firstNumber}, min = {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"max = {secondNumber}, min = {firstNumber}");
+}
